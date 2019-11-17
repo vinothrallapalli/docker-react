3601488953237750809244a510ab3cb2076756b6
vinoth.r@egovernments.org <vinoth.r@egovernments.org>
2019-11-17 21:59:37 +0530

    Sample React app with docker

---
+++ build/precache-manifest.9d4a461b95732433ba7f2f6856abe718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94000279d349c078e0b9a546fb722057",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ca0f6570b655bc9c9df",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e06f21e88b7c1ba24166",
+    "url": "/static/js/2.3f16b3fd.chunk.js"
+  },
+  {
+    "revision": "9ca0f6570b655bc9c9df",
+    "url": "/static/js/main.0b1d7023.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
